d62a40b5d46d89eeea838d69d9700ae1a51f21e0
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2023-08-01 21:22:55 +0300

    start settings

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import {BrowserRouter,Routes,Route,Link,} from "react-router-dom";
 import HomePage from './pages/HomePage';
 import CreateEventPage from './pages/CreateEventPage';
 import EventPage from './pages/EventPage';
+import eventList from './data/events.json';
 
 const App = () => {
-  
-  const events = [
-    // fetch масив з подіями
-  ];
-
-  return (
+    return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<HomePage events={events} />} />
+      <Route path="/" element={<HomePage events={eventList} />} />
       <Route path="/create-event" element={<CreateEventPage />} />
-      <Route path="/event/*" element={<EventPage />} />
+      <Route path="/event/:id" element={<EventPage />} />
     </Routes>
   </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 // index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.scss';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const rootElement = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+const rootContainer = ReactDOM.createRoot(root);
+rootContainer.render(rootElement);
+
+
+
+
